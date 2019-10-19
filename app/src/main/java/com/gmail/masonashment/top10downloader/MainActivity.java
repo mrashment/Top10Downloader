dd87d269fcfd4e810a9a692cb93ae72ae62d2df7
Mason Ashment <masonashment@gmail.com>
2019-10-18 22:08:14 -0400

    Initial commit

---
+++ app/src/main/java/com/gmail/masonashment/top10downloader/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gmail.masonashment.top10downloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class DownloadData extends AsyncTask<String,Void,String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+}
