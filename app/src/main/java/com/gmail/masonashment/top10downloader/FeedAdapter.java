4952076e8f97b3f8a49eb7aa3683c5a65cfa7cd8
Mason Ashment <masonashment@gmail.com>
2019-10-26 20:25:37 -0400

    created custom adapter to use in place of array adapter

---
+++ app/src/main/java/com/gmail/masonashment/top10downloader/FeedAdapter.java
@@ -0,0 +1,70 @@
+package com.gmail.masonashment.top10downloader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class FeedAdapter extends ArrayAdapter {
+    private static final String TAG = "FeedAdapter";
+
+    private final int layoutResource;
+    private final LayoutInflater layoutInflater;
+    private List<FeedEntry> applications;
+
+    public FeedAdapter(@NonNull Context context, int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+
+        }
+        TextView tvName = convertView.findViewById(R.id.tvName);
+        TextView tvArtist = convertView.findViewById(R.id.tvArtist);
+        TextView tvSummary = convertView.findViewById(R.id.tvSummary);
+
+        FeedEntry currentApp = applications.get(position);
+
+        tvName.setText(currentApp.getName());
+        tvArtist.setText(currentApp.getArtist());
+        tvSummary.setText(currentApp.getSummary());
+
+        return convertView;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/gmail/masonashment/top10downloader/MainActivity.java
+++ app/src/main/java/com/gmail/masonashment/top10downloader/MainActivity.java
@@ -43,8 +43,12 @@ public class MainActivity extends AppCompatActivity {
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
 
-            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.list_item, parseApplications.getApplications());
-            listApps.setAdapter(arrayAdapter);
+            //ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.list_item, parseApplications.getApplications());
+            //listApps.setAdapter(arrayAdapter);
+
+            FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this, R.layout.list_record,
+                    parseApplications.getApplications());
+            listApps.setAdapter(feedAdapter);
         }
 
         @Override
